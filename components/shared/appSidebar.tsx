5d912e07c0d454e8c4cda0298c10ab974baa727a
Evgen <ebabin000@gmail.com>
2024-10-24 14:04:20 +0300

    sidebar chechboxes

--- components/shared/appSidebar.tsx
+++ components/shared/appSidebar.tsx
@@ -11,6 +11,8 @@ import {
 import { cn } from "@/lib/utils";
 import { useIngredientsStore } from "@/store/ingredients";
 import React from "react";
+import { Checkbox } from "../ui/checkbox";
+import { Button } from "../ui/button";
 
 interface Props {
   className?: string;
@@ -18,16 +20,35 @@ interface Props {
   collapsible?: "offcanvas" | "icon" | "none";
 }
 
+export let chIng: string[]; 
+
 export function AppSidebar({
   className,
   variant = "sidebar",
   collapsible = "offcanvas",
 }: Props) {
   const { ingredients, fetchIngredients } = useIngredientsStore();
+  const [checkedIngredients, setCheckedIngredients] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     fetchIngredients();
   }, [fetchIngredients]);
+
+  React.useEffect(() => {
+    chIng = checkedIngredients
+  }, [checkedIngredients]);
+
+  const handlerChecked = (ingredientName: string) => {
+    setCheckedIngredients((prev) =>
+      prev.includes(ingredientName)
+        ? prev.filter((name) => name !== ingredientName)
+        : [...prev, ingredientName]
+    );
+  };
+
+  const handleClearChecked = () => {
+    setCheckedIngredients([]);
+  };
 
   return (
     <Sidebar
@@ -37,18 +58,31 @@ export function AppSidebar({
     >
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Ingrediants</SidebarGroupLabel>
+          <SidebarGroupLabel>Ingredients</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {ingredients.map((ingredient) => (
                 <SidebarMenuItem key={ingredient.name}>
-                  <SidebarMenuButton asChild>
-                    <a href={ingredient.imageUrl}>
-                      <span>{ingredient.name}</span>
-                    </a>
-                  </SidebarMenuButton>
+                  <a href={ingredient.imageUrl}></a>
+
+                  <div className="items-top flex space-x-2">
+                    <Checkbox
+                      id={ingredient.name}
+                      checked={checkedIngredients.includes(ingredient.name)}
+                      onCheckedChange={() => handlerChecked(ingredient.name)}
+                    />
+                    <div className="grid gap-1.5 leading-none">
+                      <label
+                        htmlFor={ingredient.name}
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        <span>{ingredient.name}</span>
+                      </label>
+                    </div>
+                  </div>
                 </SidebarMenuItem>
               ))}
+              <Button onClick={handleClearChecked}>Clear all</Button>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
