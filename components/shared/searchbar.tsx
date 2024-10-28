3c118a5ab84e2262e066fbbb892e8f837743fb6b
Evgen <ebabin000@gmail.com>
2024-10-29 00:47:42 +0200

    overflow-x searchbar fixed

--- components/shared/searchbar.tsx
+++ components/shared/searchbar.tsx
@@ -26,14 +26,14 @@ export const Searchbar: React.FC<Props> = ({ className }) => {
   return (
     <>
       {focused && (
-        <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30" />
+        <div className="fixed top-0 left-0 bottom-0 right-0  bg-black/50 z-30" />
       )}
 
       <div
         ref={ref}
         className={cn(
           "flex rounded-2xl flex-1 justify-between relative h-11 z-30 ",
-          isMobile && focused && "absolute w-[calc(100vw-40px)]",
+          isMobile && focused && "absolute w-[calc(100vw-20px)]",
           className
         )}
       >
@@ -48,7 +48,7 @@ export const Searchbar: React.FC<Props> = ({ className }) => {
           className={cn(
             "rounded-2xl outline-none w-full bg-gray-100 pl-11",
             isMobile && "w-full pl-8 text-sm",
-            focused && isMobile && "w-[calc(100vw-40px)] pl-11"
+            focused && isMobile && "w-screen pl-11"
           )}
           type="text"
           placeholder="Search..."
@@ -60,10 +60,11 @@ export const Searchbar: React.FC<Props> = ({ className }) => {
         {products.length > 0 && (
           <div
             className={cn(
-              "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all invisible opacity-0 z-30",
-              focused && "opacity-100 top-12 duration-0 visible translate-y-0",
-              isMobile && "w-[calc(100vw-40px)] left-0",
-              !focused && isMobile ? "duration-0" : "duration-200"
+              "absolute w-full right-0 lef bg-white rounded-xl py-2 top-14 shadow-md transition-all hidden opacity-0 z-30",
+              focused && "opacity-100 top-12 duration-0 block translate-y-0",
+              isMobile && "w-full left-0",
+              !focused && isMobile ? "duration-0" : "duration-200",
+              focused && isMobile && ""
             )}
           >
             {products.map((product) => (
